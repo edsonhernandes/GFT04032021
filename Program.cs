a3c135c94521e8c3fdcdf05fe65c38daef408c30
edsonhernandes <71903564+edsonhernandes@users.noreply.github.com>
2021-03-04 16:11:21 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GFT04032021_exerc_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = 0;
+            while (valor < 3)
+            {
+                Console.WriteLine(valor);
+                Console.WriteLine("Incomoda");
+                valor = valor+1;
+
+            }
+
+            Console.WriteLine(" Eu quero entrar na GFT para aprender fazer melhor!");
+            Console.ReadLine();
+
+        }
+        
+    }
+}
