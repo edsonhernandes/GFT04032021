829a92b4cd46a4576b5f7e1bb0bf8b99c8866f5f
edsonhernandes <71903564+edsonhernandes@users.noreply.github.com>
2021-03-04 16:12:44 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,25 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GFT04032021_exerc_4
+namespace GFT04032021
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int valor = 0;
-            while (valor < 3)
+            int maior = 0, menor, i = 0, numero = 0, tl = 0;
+            Console.WriteLine("Determina o maior e menor número informado pelo usuario!");
+            Console.WriteLine("Deseja informar quantos números? ");
+            tl = Convert.ToInt32(Console.ReadLine());   
+                
+            Console.Write("Informe o primeiro numero: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+            maior = numero;
+            menor = numero;
+            i = 1;
+
+            for (i = 2; i <= tl; i++) ;
             {
-                Console.WriteLine(valor);
-                Console.WriteLine("Incomoda");
-                valor = valor+1;
+                Console.Write("Informe o "+i+" numero: ");
+                numero = Convert.ToInt32(Console.ReadLine());
+                if (numero < menor)
+                {
+                    menor = numero;
+                }
+
+                if (numero > maior)
+                {
+                    maior = numero;
+                }
 
             }
 
-            Console.WriteLine(" Eu quero entrar na GFT para aprender fazer melhor!");
-            Console.ReadLine();
 
+            Console.WriteLine("O maior número é: "+ maior );
+            Console.WriteLine("O menor número é: "+ menor );
+            
+            Console.ReadKey();
         }
-        
     }
 }
